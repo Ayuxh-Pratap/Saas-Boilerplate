c1050cba90721acb078f7c392a2175268ffab18f
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-05-06 20:50:22 +0530

    first commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@/components/ui/button'
+import { KeyRound } from 'lucide-react'
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='flex items-center justify-center w-full h-screen'>
+            <div className='w-96 rounded-md border p-5 space-y-5 relative bg-slate-900'>
+                <div className='flex items-center gap-2'>
+                    <KeyRound />
+                    <h1 className='text-2xl font-bold'>Next + Supabase</h1>
+                </div>
+                <p className='text-sm text-gray-300'>Register/SignIn Today 👇</p>
+
+                <div className='flex flex-col gap-5'>
+                <Button className='flex items-center gap-2 w-full' variant="outline"> <FaGithub /> Github</Button>
+                <Button className='flex items-center gap-2 w-full' variant="outline"> <FcGoogle /> Google</Button>
+                </div>
+                <div className='glowbox opacity-15 -z-10'></div>
+
+            </div>
+        </div>
+    )
+}
