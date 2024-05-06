c97d1c2665727a01abbc7207b7ff3f0fdd920cfa
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-05-07 04:56:16 +0530

    OAuth route

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Button>
+        <a href="/auth">click me</a>
+      </Button>
     </div>
   )
 }
