138b58cf7f69fb6265dda8732ae9c6e4d692ca17
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-05-07 04:17:28 +0530

    git provider function

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,6 +5,11 @@ import { FcGoogle } from "react-icons/fc";
 import React from 'react'
 
 export default function page() {
+
+    const handleLoginWithOAuth = (provider: "github" | "google") => {
+
+    }
+
     return (
         <div className='flex items-center justify-center w-full h-screen'>
             <div className='w-96 rounded-md border p-5 space-y-5 relative bg-slate-900'>
@@ -15,7 +20,7 @@ export default function page() {
                 <p className='text-sm text-gray-300'>Register/SignIn Today 👇</p>
 
                 <div className='flex flex-col gap-5'>
-                <Button className='flex items-center gap-2 w-full' variant="outline"> <FaGithub /> Github</Button>
+                <Button className='flex items-center gap-2 w-full' variant="outline" onClick={ handleLoginWithOAuth }> <FaGithub /> Github</Button>
                 <Button className='flex items-center gap-2 w-full' variant="outline"> <FcGoogle /> Google</Button>
                 </div>
                 <div className='glowbox opacity-15 -z-10'></div>
